438b967a0b5b0e5d6b214fd7c229d55a07c28c60
Eduardo Henrique <eduardo.h.f@hotmail.com>
2018-07-01 11:58:53 -0300

    pages different per user

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'contribute' => 'pages#contribute'
   get 'about' => 'pages#about'
   get 'faq' => 'pages#faq'
+  get 'user/:id' => 'pages#contribuicoes'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
